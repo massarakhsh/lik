01ca68aa83113d93a581fa68c485bd7ed79d93a4
Shaman <shamaner.17@gmail.com>
2021-01-10 23:44:43 +0300

    Shaman

--- likref.js
+++ likref.js
@@ -1,73 +1,102 @@
+const likRefId = 'SysNum';
 let likRefRegister = {};
-let likRefIndex = 0;
 let likRefTick = null;
 
-function lik_ref_initialize() {
-    likRefRegister = {};
-    likRefIndex = 0;
-    lik_ref_request();
-    if (!likRefTick) {
-        likRefTick = setInterval(lik_ref_request, 10000);
+class LikRefTable {
+    Table = "";
+    Index = 0;
+    Elms = {};
+    callBacks = [];
+
+    constructor(table) {
+        this.Table = table;
+    }
+
+    get(callback) {
+        if (this.Index > 0) {
+            setTimeout(() => callback(this.Elms), 1);
+        } else {
+            this.callBacks.push(callback);
+        }
+    }
+
+    set(elms, index) {
+        this.Index = index;
+        if (elms) {
+            for (var id in elms) {
+                let elm = elms[id];
+                let old = this.Elms[id];
+                if (old && (!elm || !elm[likRefId])) {
+                    delete this.elms[id];
+                } else if (elm && elm[likRefId] == id) {
+                    this.Elms[id] = elm;
+                }
+            }
+        }
+        while (this.callBacks.length > 0) {
+            let callback = this.callBacks.shift();
+            this.get(callback);
+        }
     }
 }
 
-function lik_ref_request() {
-    lik_ref_call("GET", null, 'http://localhost:8090/api/marshal/' + likRefIndex);
+function _lik_ref_tick() {
+    if (!likRefTick) {
+        likRefTick = setInterval(() => _lik_ref_request_all(), 10000);
+        _lik_ref_request_all();
+    }
 }
 
-function lik_ref_call(type, data, url) {
+function _lik_ref_request_all() {
+    for (let table in likRefRegister) {
+        _lik_ref_request_table(table, likRefRegister[table].Index);
+    }
+}
+
+function _lik_ref_request_table(table, index) {
+    let url = 'http://localhost:8090/api/' + table + '/' + index;
     jQuery.ajax({
-        type: type,
+        type: 'GET',
         url: url,
-        data: data,
         dataType: "json",
         crossDomain: true,
-        timeout: 10000,
+        timeout: 5000,
         success: function(code) {
-            lik_ref_update(code);
+            _lik_ref_update(code);
         }
     });
 }
 
-function lik_ref_update(lika) {
+function _lik_ref_update(lika) {
     if (lika) {
-        for (var key in lika) {
-            if (key == 'index') {
-                likRefIndex = lika[key];
-            } else {
-                let tin = lika[key];
-                let tis = likRefRegister[key];
-                if (!tis) {
-                    tis = {};
-                    likRefRegister[key] = tis;
+        const index = lika['index'];
+        for (let key in lika) {
+            if (key != 'index') {
+                let reg = likRefRegister[key];
+                if (!reg) {
+                    reg = new LikRefTable(key);
+                    likRefRegister[key] = reg;
                 }
-                if (tin) {
-                    for (var id in tin) {
-                        let ein = tin[id];
-                        let eis = tis[id];
-                        if (eis && (!ein || !ein["SysNum"])) {
-                            delete tis[id];
-                        } else if (ein && ein["SysNum"] == id) {
-                            tis[id] = ein;
-                        }
-                    }
-                }
+                reg.set(lika[key], index);
             }
         }
     }
 }
 
-function lik_ref_get(path) {
-    let names = path.split('/');
-    let loc = likRefRegister;
-    for (let p = 0; p < names.length; p++) {
-        let name = names[p];
-        if (name) {
-            let dat = loc[name];
-            if (typeof(dat) === 'undefined' || dat === null) return null;
-            loc = dat;
-        }
+function likref_get_all(table, callback) {
+    _lik_ref_tick();
+    let reg = likRefRegister[table];
+    if (!reg) {
+        reg = new LikRefTable(table);
+        likRefRegister[table] = reg;
+        _lik_ref_request_table(table, 0);
     }
-    return loc;
+    reg.get(callback);
+}
+
+function likref_get_sys(table, sys, callback) {
+    likref_get_all(table, (elms) => {
+        callback(elms[sys]);
+    });
 }
 
