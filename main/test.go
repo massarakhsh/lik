5406307ead05cf50755e9cb40f58da5593f5788f
Мурсалов Станислав Мурсалович <sm_mursalov@betboom.org>
2023-05-07 23:01:15 +0300

    #patch

--- main/test.go
+++ main/test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 
 	"github.com/massarakhsh/lik"
 )
@@ -10,47 +9,15 @@ import (
 type TP struct {
 	Name   string
 	Format string
+	Lst    []string
 }
 
 func main() {
 	data := TP{Name: "Имя", Format: "Форма"}
+	data.Lst = []string{"Раз", "Два"}
 	set := lik.BuildSet(data)
 	fmt.Println(set.Serialize())
-	data1 := SetToStruct[TP](set)
-	if data == data1 {
-		fmt.Println("Ok")
-	} else {
-		fmt.Println("Fault")
-	}
-}
-
-func SetToStruct[T any](set lik.Seter) T {
-	var res T
-	result := &res
-	tp := reflect.TypeOf(result)
-	vl := reflect.ValueOf(result)
-	if vl.Kind() != reflect.Pointer || vl.IsNil() {
-		return res
-	}
-	tp = tp.Elem()
-	vl = vl.Elem()
-	cnt := vl.NumField()
-	for f := 0; f < cnt; f++ {
-		name := tp.Field(f).Name
-		item := set.GetItem(name)
-		if item == nil {
-			nm := lik.ToSnakeCase(name)
-			item = set.GetItem(nm)
-		}
-		if item != nil {
-			if val := vl.Field(f); val.IsValid() {
-				if stp := tp.Field(f).Type.Kind().String(); stp == "string" {
-					val.Set(reflect.ValueOf(item.ToString()))
-				} else {
-					_ = stp
-				}
-			}
-		}
-	}
-	return res
+	data1 := lik.SetToStruct[TP](set)
+	_ = data1
+	fmt.Println("Ok")
 }
--- parse.go
+++ parse.go
@@ -1,9 +1,6 @@
 package lik
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
 const (
 	Dbg  = false
@@ -24,61 +21,6 @@ type JsonParser interface {
 	scanItImmediate() string
 	getNextRune() rune
 	stepNextRule()
-}
-
-func StrToQuotes(str string) string {
-	val := strings.Replace(str, "\\", "\\\\", -1)
-	val = strings.Replace(val, "\"", "\\\"", -1)
-	val = strings.Replace(val, "\n", "\\n", -1)
-	val = strings.Replace(val, "\r", "\\r", -1)
-	return "\"" + val + "\""
-}
-
-func SetFromRequest(data string) Seter {
-	result := BuildSet()
-	data = strings.Trim(data, " \n\r\t\b")
-	if data == "" {
-	} else if strings.HasPrefix(data, "{") {
-		pars := buildParse(data)
-		result = pars.scanItMap()
-	} else {
-		words := strings.Split(data, "&")
-		for _, word := range words {
-			if peq := strings.Index(word, "="); peq > 0 {
-				key := word[0:peq]
-				val := word[peq+1:]
-				pars := buildParse(val)
-				item := pars.scanItValue()
-				if key != "" {
-					result.SetValue(key, item)
-				}
-			}
-		}
-	}
-	return result
-}
-
-func SetFromMap(data map[string]interface{}) Seter {
-	result := BuildSet()
-	for key, val := range data {
-		result.SetValue(key, val)
-	}
-	return result
-}
-
-func SetFromStruct(data interface{}) Seter {
-	return BuildItem(data).ToSet()
-}
-
-func ListFromRequest(data string) Lister {
-	result := BuildList()
-	data = strings.Trim(data, " \n\r\t")
-	if data == "" {
-	} else if strings.HasPrefix(data, "[") {
-		pars := buildParse(data)
-		result = pars.scanItList()
-	}
-	return result
 }
 
 func buildParse(data string) *JsonParse {
