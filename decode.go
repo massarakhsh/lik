01322bc656a6234b5506ec568b252617be1a625d
Мурсалов Станислав Мурсалович <shamaner.17@gmail.com>
2023-05-12 11:14:31 +0300

    Add ItemFromFile

--- decode.go
+++ decode.go
@@ -1,29 +1,76 @@
 package lik
 
-import "strings"
+import (
+	"os"
+	"strings"
+)
+
+func SetFromFile(filename string) Seter {
+	var set Seter
+	if item := ItemFromFile(filename); item != nil {
+		set = item.ToSet()
+	}
+	return set
+}
+
+func ItemFromFile(filename string) Itemer {
+	var item Itemer
+	if data, err := os.ReadFile(filename); err == nil {
+		item = ItemFromString(string(data))
+	}
+	return item
+}
+
+func SetFromString(data string) Seter {
+	var set Seter
+	if item := ItemFromString(data); item != nil {
+		set = item.ToSet()
+	}
+	return set
+}
+
+func ItemFromString(data string) Itemer {
+	var item Itemer
+	str := strings.Trim(data, " \n\r\t\b")
+	if strings.HasPrefix(str, "{") {
+		pars := buildParse(str)
+		item = pars.scanItMap()
+	} else if strings.HasPrefix(str, "[") {
+		pars := buildParse(str)
+		item = pars.scanItList()
+	} else {
+		item = BuildItem(data)
+	}
+	return item
+}
 
 func SetFromRequest(data string) Seter {
-	result := BuildSet()
+	set := BuildSet()
 	data = strings.Trim(data, " \n\r\t\b")
-	if data == "" {
-	} else if strings.HasPrefix(data, "{") {
-		pars := buildParse(data)
-		result = pars.scanItMap()
+	if strings.HasPrefix(data, "{") {
+		set = SetFromString(data)
 	} else {
-		words := strings.Split(data, "&")
-		for _, word := range words {
-			if peq := strings.Index(word, "="); peq > 0 {
-				key := word[0:peq]
-				val := word[peq+1:]
-				pars := buildParse(val)
-				item := pars.scanItValue()
-				if key != "" {
-					result.SetValue(key, item)
-				}
+		set = SetFromQuery(data)
+	}
+	return set
+}
+
+func SetFromQuery(data string) Seter {
+	set := BuildSet()
+	data = strings.Trim(data, " \n\r\t\b")
+	words := strings.Split(data, "&")
+	for _, word := range words {
+		if peq := strings.Index(word, "="); peq > 0 {
+			key := word[0:peq]
+			val := word[peq+1:]
+			pars := buildParse(val)
+			item := pars.scanItValue()
+			if key != "" {
+				set.SetValue(key, item)
 			}
 		}
 	}
-	return result
+	return set
 }
 
 func SetFromMap(data map[string]interface{}) Seter {
