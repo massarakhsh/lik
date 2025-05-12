e2c338db06c1c0ff276ee1116f34d3cb30775e33
Мурсалов Станислав Мурсалович <sm_mursalov@betboom.org>
2025-05-12 09:02:31 +0300

    Тест

--- item_test.go
+++ item_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestItem(t *testing.T) {
-	data := `{"str":"str", "yes":true, "no":false, "digit":{"dva":2,"nepolt":-1.5}, "array":[11, "12", 13]}`
+	data := `{"str":"str", "yes":true, "no":false, "net":null, "digit":{"dva":2,"nepolt":-1.5}, "array":[11, "12", null, 13]}`
 	item := ItemFromString(data)
 	if item == nil {
 		t.Errorf("ERROR of ItemFromString(): noitem")
@@ -21,8 +21,10 @@ func TestItem(t *testing.T) {
 		assert.Equal(t, "str", set.GetString("str"))
 		assert.Equal(t, false, set.GetBool("no"))
 		assert.Equal(t, true, set.GetBool("yes"))
+		assert.Nil(t, set.GetItem("net"))
 		assert.Equal(t, 2, int(set.GetInt("digit/dva")))
 		assert.Equal(t, -1.5, set.GetFloat("digit/nepolt"))
+		assert.Equal(t, 3, int(set.GetList("array").Count()))
 		assert.Equal(t, 11, int(set.GetInt("array/0")))
 		assert.Equal(t, 12, int(set.GetInt("array/1")))
 		assert.Equal(t, "12", set.GetString("array/1"))
